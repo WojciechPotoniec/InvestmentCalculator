0328020517388e3732546074542df84b1406da60
Wojciech Potoniec <155390018+WojciechPotoniec@users.noreply.github.com>
2025-03-27 19:45:50 +0100

    add condition to diplay content

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,10 @@ function App() {
       initialInvestment: 0,
       annualInvestment: 0,
       expectedReturn: 0,
-      duration: 0,
+      duration: 1,
     });
+
+  const inputIsValid = userInput.duration >= 1;
 
     function handleChange(inputIdentifier, newValue) {
       setUserInput((prevUserInput) => {
@@ -25,7 +27,8 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange}/>
-      <Results input={userInput}/>
+      {!inputIsValid && <p className="center">Please enter a duration greater than zero.</p>}
+      {inputIsValid && <Results input={userInput}/>}
     </>
   );
 }
